units test fixed

src/pymarine/utils/date_time.py
@@ -9,11 +9,16 @@ def _from_matlab_to_python_num_days(x):
     # matlab epoch time start at the year 0 , python at the 1-1-0001. Both
     # formats differ 366 days. So to convert the matlab format to the python
     # format, convert it here
-    return datetime.datetime.fromordinal(int(x)) + datetime.timedelta(
-        days=x % 1) - datetime.timedelta(days=366)
+    return (
+        datetime.datetime.fromordinal(int(x))
+        + datetime.timedelta(days=x % 1)
+        - datetime.timedelta(days=366)
+    )
 
 
-_from_matlab_to_python_num_days_vectorized = np.vectorize(_from_matlab_to_python_num_days)
+_from_matlab_to_python_num_days_vectorized = np.vectorize(
+    _from_matlab_to_python_num_days
+)
 
 
 def matlabnum2date(x):
@@ -70,68 +75,8 @@ def matlabnum2date(x):
     return number_of_days
 
 
-def _datetime2matlabdn(dt):
-    # low level implementation of the python to matlab numerical date time convention. In principal
-    # just 360 days are added to the input value
-    mdn = dt + datetime.timedelta(days=366)
-    delta_date = dt - datetime.datetime(dt.year, dt.month, dt.day, 0, 0, 0)
-    frac_seconds = delta_date.seconds / (24.0 * 60.0 * 60.0)
-    frac_microseconds = dt.microsecond / (24.0 * 60.0 * 60.0 * 1000000.0)
-    return mdn.toordinal() + frac_seconds + frac_microseconds
-
-
-def datenum2matlabnumdn(x):
-    """Convert date/time representation in number of days since 0001-01 00:00:00 (Python convention)
-    to number of days since 0000-00 00:00:00 (Matlab convention)
-
-    Parameters
-    ----------
-    x : float or ndarray
-        Date/time representation for Python system with the number of days since 0001 01 00:00:00
-
-    Returns
-    -------
-    date_time_num_matlab : ndarray or scalar
-        Numerical date/time representation in the Matlab convention corresponding to the numerical
-        date/time representation `x` in the Python convention
-
-    Examples
-    --------
-    First obtain a date/time in numerical representation. We use the matplotlib.dates.date2num
-    function for this.
-
-    >>> from matplotlib.dates import date2num
-    >>> date_time = datetime.datetime(2012, 12, 21, 12, 12, 12)
-    >>> num_date_python = date2num(date_time)
-    >>> print("{:.7e}".format(num_date_python))
-    7.3485851e+05
-
-    This value is the Python numerical data/time definition, ie. represents the number of days since
-    0001-01 00:00:00
-
-    In order to convert this to the Matlab numerical date time definition we can do
-
-    >>> num_date_matlab_conv = datenum2matlabnumdn(num_date_python)
-    >>> print("{:.7e}".format(num_date_matlab_conv))
-    7.3522451e+05
-
-    which represents the number of days since 0000-00 00:00:00. Note that the difference between the
-    two definitions is indeed 366 days:
-
-    >>> num_date_matlab_conv - num_date_python
-    366.0
-
-    This is one year (365 days) plus one, since Matlab starts at January 0, Python at January 1
-
-    """
-    dt = datetime.datetime.fromordinal(int(x)) + datetime.timedelta(days=x % 1)
-    date_time_num_matlab = _datetime2matlabdn(dt)
-
-    return date_time_num_matlab
-
-
 def valid_date(s):
-    """ Check if supplied data *s* is a valid date for the format Year-Month-Day
+    """Check if supplied data *s* is a valid date for the format Year-Month-Day
 
     Parameters
     ----------

src/pymarine/utils/geographic.py
@@ -1,13 +1,15 @@
 """
 Collection of functions dealing with geographical coordinates based on the *LatLon* module.
 """
+import logging
 import fastkml as kml
 import numpy as np
 import pandas as pd
-from LatLon.LatLon import (LatLon)
+from latloncalc.latlon import LatLon
 from scipy.constants import nautical_mile
 
-from hmc_utils.misc import get_logger
+
+_logger = logging.getLogger(__name__)
 
 
 class LocationCheck(object):
@@ -33,7 +35,6 @@ class LocationCheck(object):
     """
 
     def __init__(self, longitude, latitude, distance_deviation_allowed=10):
-        self.logger = get_logger(__name__)
         self.target_location = LatLon(latitude, longitude)
         self.distance_deviation_allowed = float(distance_deviation_allowed)
         self.distance = None
@@ -105,12 +106,16 @@ class LocationCheck(object):
 
         self.distance = self.current_location.distance(self.target_location)
 
-        self.logger.debug("Distance forecast location {} with target location {}: {}"
-                          "".format(self.current_location, self.target_location, self.distance))
+        _logger.debug(
+            "Distance forecast location {} with target location {}: {}"
+            "".format(self.current_location, self.target_location, self.distance)
+        )
 
         if self.distance > self.distance_deviation_allowed:
-            self.logger.debug("Distance {} out of range of {} with {} km"
-                              "".format(self.distance, self.target_location, self.distance))
+            _logger.debug(
+                "Distance {} out of range of {} with {} km"
+                "".format(self.distance, self.target_location, self.distance)
+            )
             out_of_range = True
         else:
             out_of_range = False
@@ -120,14 +125,24 @@ class LocationCheck(object):
     def make_report(self):
 
         msg = "{:20s} : {} {}"
-        self.logger.info(msg.format("Target location set at", self.target_location, ""))
-        self.logger.info(msg.format("Maximum distance deviation allowed",
-                                    self.distance_deviation_allowed, " km"))
+        _logger.info(msg.format("Target location set at", self.target_location, ""))
+        _logger.info(
+            msg.format(
+                "Maximum distance deviation allowed",
+                self.distance_deviation_allowed,
+                " km",
+            )
+        )
 
 
-def get_speed_from_distance_and_time(trajectory, distance_name="travel_distance",
-                                     speed_name="speed_sustained", datetime_name="DateTime",
-                                     travel_time_name="travel_time", speed_max_clip=None):
+def get_speed_from_distance_and_time(
+    trajectory,
+    distance_name="travel_distance",
+    speed_name="speed_sustained",
+    datetime_name="DateTime",
+    travel_time_name="travel_time",
+    speed_max_clip=None,
+):
     """Calculate the speed based on the travel distance and travel time.
 
     Parameters
@@ -201,7 +216,7 @@ def get_speed_from_distance_and_time(trajectory, distance_name="travel_distance"
     # make sure we are dealing with date/time, not strings
     date_time = pd.Series([pd.Timestamp(t) for t in date_time])
 
-    time_in_hour = ((date_time - date_time[0]) / pd.Timedelta('1 hour'))
+    time_in_hour = (date_time - date_time[0]) / pd.Timedelta("1 hour")
     delta_time = time_in_hour.diff().fillna(1).values
     trajectory[travel_time_name] = time_in_hour.values
     trajectory[speed_name] = trajectory[distance_name].diff().fillna(0) / delta_time
@@ -225,10 +240,13 @@ def get_speed_from_distance_and_time(trajectory, distance_name="travel_distance"
     return trajectory
 
 
-def travel_distance_and_heading_from_coordinates(db, latitude_name="GPS_LATITUDE",
-                                                 longitude_name="GPS_LONGITUDE",
-                                                 heading_name="HEADING",
-                                                 travel_distance_name="travel_distance"):
+def travel_distance_and_heading_from_coordinates(
+    db,
+    latitude_name="GPS_LATITUDE",
+    longitude_name="GPS_LONGITUDE",
+    heading_name="HEADING",
+    travel_distance_name="travel_distance",
+):
     """
     Calculate the travel distance and optionally the heading based on the latitude and longitude
     columns in the DataFrame `db`
@@ -308,7 +326,6 @@ def travel_distance_and_heading_from_coordinates(db, latitude_name="GPS_LATITUDE
 
     """
 
-    log = get_logger(__name__)
 
     n_rows = db.index.size
 
@@ -323,17 +340,17 @@ def travel_distance_and_heading_from_coordinates(db, latitude_name="GPS_LATITUDE
     else:
         # check if headings is not empty
         if True in db[heading_name].isnull():
-            log.debug("empty heading. filling it with zeros")
+            _logger.debug("empty heading. filling it with zeros")
             headings = np.zeros(n_rows)
 
-    log.debug("Start loop {} ".format(LatLon))
+    _logger.debug("Start loop {} ".format(LatLon))
     # loop over all the lines
     for i in range(n_rows):
         # get the coordinates from the GPS_LATITUDE and GPS_LONGITUDE columns and turn into a LatLon
         # object
         latitude = db[latitude_name].values[i]
         longitude = db[longitude_name].values[i]
-        log.debug("Converting {} {} with {}".format(latitude, longitude, LatLon))
+        _logger.debug("Converting {} {} with {}".format(latitude, longitude, LatLon))
         coordinates = LatLon(latitude, longitude)
 
         if last_coordinates is None:
@@ -343,7 +360,9 @@ def travel_distance_and_heading_from_coordinates(db, latitude_name="GPS_LATITUDE
             # for all other row, calculate the travel distance wro the last position. Convert to
             # miles
             try:
-                displacement = coordinates.distance(last_coordinates) / (nautical_mile / 1000.0)
+                displacement = coordinates.distance(last_coordinates) / (
+                    nautical_mile / 1000.0
+                )
             except ValueError:
                 displacement = 0
 
@@ -369,9 +388,15 @@ def travel_distance_and_heading_from_coordinates(db, latitude_name="GPS_LATITUDE
     return db
 
 
-def import_way_points(file_name, latitude_name="latitude", longitude_name="longitude",
-                      heading_name="heading", travel_distance_name="travel_distance",
-                      n_distance_points=2000, start_wp=None):
+def import_way_points(
+    file_name,
+    latitude_name="latitude",
+    longitude_name="longitude",
+    heading_name="heading",
+    travel_distance_name="travel_distance",
+    n_distance_points=2000,
+    start_wp=None,
+):
     """Import the way points of a Google Earth kml file.
 
     Parameters
@@ -470,7 +495,6 @@ def import_way_points(file_name, latitude_name="latitude", longitude_name="longi
 
     """
 
-    logger = get_logger(__name__)
 
     # open the google earth kml file. Do it as binary in order to avoid decoding issues
     with open(file_name, "rb") as fp:
@@ -486,40 +510,57 @@ def import_way_points(file_name, latitude_name="latitude", longitude_name="longi
             for cnt, k_level3 in enumerate(k_level2.features()):
                 point = k_level3.geometry
                 if start_wp is not None and cnt < start_wp:
-                    logger.debug("Skipping Way point {}: lat = {} lon = {}"
-                                 "".format(cnt, point.x, point.y))
+                    _logger.debug(
+                        "Skipping Way point {}: lat = {} lon = {}"
+                        "".format(cnt, point.x, point.y)
+                    )
                     continue
                 longitudes.append(point.x)
                 latitudes.append(point.y)
-                logger.debug("Adding Way point {}: lat = {} lon = {}"
-                             "".format(cnt, latitudes[-1], longitudes[-1]))
+                _logger.debug(
+                    "Adding Way point {}: lat = {} lon = {}"
+                    "".format(cnt, latitudes[-1], longitudes[-1])
+                )
 
     # create data frame out of way point coordinates and return
-    df = pd.DataFrame(data=np.vstack((latitudes, longitudes)).T,
-                      columns=[latitude_name, longitude_name])
+    df = pd.DataFrame(
+        data=np.vstack((latitudes, longitudes)).T,
+        columns=[latitude_name, longitude_name],
+    )
 
     # calculate the travel distance
-    df = travel_distance_and_heading_from_coordinates(df, latitude_name=latitude_name,
-                                                      longitude_name=longitude_name,
-                                                      heading_name=heading_name,
-                                                      travel_distance_name=travel_distance_name)
+    df = travel_distance_and_heading_from_coordinates(
+        df,
+        latitude_name=latitude_name,
+        longitude_name=longitude_name,
+        heading_name=heading_name,
+        travel_distance_name=travel_distance_name,
+    )
 
     if n_distance_points is not None:
         df.set_index(travel_distance_name, inplace=True, drop=False)
         distances = np.linspace(0, df.index[-1], n_distance_points, endpoint=True)
-        df2 = pd.DataFrame(index=distances, columns=[latitude_name, longitude_name, heading_name])
+        df2 = pd.DataFrame(
+            index=distances, columns=[latitude_name, longitude_name, heading_name]
+        )
         df2[travel_distance_name] = distances
         df3 = pd.concat([df, df2]).sort_index().interpolate()
-        df3 = df3.reset_index().drop_duplicates(
-            keep="first", subset=travel_distance_name).set_index(travel_distance_name)
+        df3 = (
+            df3.reset_index()
+            .drop_duplicates(keep="first", subset=travel_distance_name)
+            .set_index(travel_distance_name)
+        )
         df3 = df3.reindex(df2.index)
         df3.reset_index(inplace=True, drop=True)
         df3.drop("index", axis=1, inplace=True)
 
-        df = travel_distance_and_heading_from_coordinates(df3, latitude_name=latitude_name,
-                                                          longitude_name=longitude_name,
-                                                          heading_name=heading_name,
-                                                          travel_distance_name=travel_distance_name)
+        df = travel_distance_and_heading_from_coordinates(
+            df3,
+            latitude_name=latitude_name,
+            longitude_name=longitude_name,
+            heading_name=heading_name,
+            travel_distance_name=travel_distance_name,
+        )
 
     df.set_index(travel_distance_name, inplace=True, drop=True)
 

src/pymarine/utils/misc.py
@@ -1,5 +1,5 @@
 """
-Some miscellaneous functions used throughout many hmc modules
+Some miscellaneous functions
 """
 import logging
 import os
@@ -11,6 +11,8 @@ import time
 from builtins import input
 from builtins import object
 from builtins import str
+
+logger = logging.getLogger(__name__)
 
 try:
     import dateutil.parser as dparser
@@ -45,7 +47,7 @@ except ImportError:
     yamlordereddictloader = None
 
 try:
-    from hmc_utils import Q_
+    from pymarine.utils import Q_
 except ImportError:
     Q_ = None
 
@@ -131,7 +133,6 @@ class Timer(object):
 
     def __init__(self, message="Elapsed time", name="routine", verbose=True, units='ms', n_digits=0,
                  field_width=20):
-        self.logger = get_logger(__name__)
         self.verbose = verbose
         self.message = message
         self.name = name
@@ -163,95 +164,15 @@ class Timer(object):
         self.secs = float(self.delta_time / np.timedelta64(1, "s"))
 
         # debug output
-        self.logger.debug("Found delta time in ns: {}".format(self.delta_time))
+        logger.debug("Found delta time in ns: {}".format(self.delta_time))
 
         if self.verbose:
             # convert the delta time to the desired units
             duration = self.delta_time / np.timedelta64(1, self.units)
 
             # produce output
-            self.logger.info(self.format_string.format(self.message, self.name, duration,
+            logger.info(self.format_string.format(self.message, self.name, duration,
                                                        self.units))
-
-
-def get_logger(name):
-    """Get the logger of the current level and set the level based on the main routine. Then return
-    it
-
-    Parameters
-    ----------
-    name : str
-        the name of the logger to set.
-
-    Returns
-    -------
-    type
-        log: a handle of the current logger
-
-    Notes
-    -----
-    This routine is used on top of each function to get the handle to the current logger and
-    automatically set the verbosity level of the logger based on the main function
-
-    Examples
-    --------
-
-    Assume you define a function which need to generate logging information based on the logger
-    created in the main program. In that case you can do
-
-    >>> def small_function():
-    ...    logger = get_logger(__name__)
-    ...    logger.info("Inside 'small_function' This is information to the user")
-    ...    logger.debug("Inside 'small_function' This is some debugging stuff")
-    ...    logger.warning("Inside 'small_function' This is a warning")
-    ...    logger.critical("Inside 'small_function' The world is collapsing!")
-
-    The logger can be created in the main program using the create_logger routine
-
-    >>> def main(logging_level):
-    ...     main_logger = create_logger(console_log_level=logging_level)
-    ...     main_logger.info("Some information in the main")
-    ...     main_logger.debug("Now we are calling the function")
-    ...     small_function()
-    ...     main_logger.debug("We are back in the main function")
-
-    Let's call the main fuction in DEBUGGING mode
-
-    >>> main(logging.DEBUG)
-      INFO : Some information in the main
-     DEBUG : Now we are calling the function
-      INFO : Inside 'small_function' This is information to the user
-     DEBUG : Inside 'small_function' This is some debugging stuff
-    WARNING : Inside 'small_function' This is a warning
-    CRITICAL : Inside 'small_function' The world is collapsing!
-     DEBUG : We are back in the main function
-
-
-    You can see that the logging level inside the `small_function` is obtained from the main level.
-    Do the same but now in the normal information mode
-
-    >>> main(logging.INFO)
-      INFO : Some information in the main
-      INFO : Inside 'small_function' This is information to the user
-    WARNING : Inside 'small_function' This is a warning
-    CRITICAL : Inside 'small_function' The world is collapsing!
-
-    We can call in the silent mode, suppressing all debugging and normal info, but not Warnings
-
-    >>> main(logging.WARNING)
-    WARNING : Inside 'small_function' This is a warning
-    CRITICAL : Inside 'small_function' The world is collapsing!
-
-    Finally, to suppress everything except for critical warnings
-
-    >>> main(logging.CRITICAL)
-    CRITICAL : Inside 'small_function' The world is collapsing!
-
-    """
-    # the logger is based on the current main routine
-    log = logging.getLogger(name)
-    log.setLevel(logging.getLogger("__main__").getEffectiveLevel())
-    return log
 
 
 def is_exe(fpath):
@@ -965,6 +886,7 @@ def set_default_dimension(parse_value, default_dimension=None, force_default_uni
 
     Returns
     -------
+
     :obj:`Quantity`
         Value with the quantity as give by the default
 
@@ -997,7 +919,6 @@ def set_default_dimension(parse_value, default_dimension=None, force_default_uni
     default dimension of *meter* to it in case the value do not have an explicit dimension yet.
     Just do
 
-    >>> logger = create_logger(console_log_level=logging.CRITICAL)
     >>> value_without_dimension = 1.0  # this is the values as we read from the text file
     >>> value_with_dimension = set_default_dimension(value_without_dimension, "meter")
     >>> print(value_with_dimension)
@@ -1036,12 +957,11 @@ def set_default_dimension(parse_value, default_dimension=None, force_default_uni
 
     Notes
     -----
-    * Hz are not converted to rad/s as expected. Therefore do not try to use this to convert
+    * Hz are not converted to rad/s as expected. Therefore, do not try to use this to convert
       Hz -> rad/s
     * If the input argument *parse_val* is None, a None is returned as output as well
 
     """
-    logger = get_logger(__name__)
     if default_dimension is not None:
         def_unit_val = Q_(1, default_dimension)
     else:

tests/test_coordinate_transformations.py
@@ -1,27 +1,33 @@
 import numpy as np
-from numpy import (pi, sqrt, degrees)
-from numpy.testing import (assert_almost_equal)
+from numpy import pi, sqrt, degrees
+from numpy.testing import assert_almost_equal
 
-from hmc_utils.coordinate_transformations import (cartesian_to_cylindrical,
-                                                  cylindrical_to_cartesian,
-                                                  cartesian_to_spherical, spherical_to_cartesian,
-                                                  latitude_to_colatitude, colatitude_to_latitude)
+from pymarine.utils.coordinate_transformations import (
+    cartesian_to_cylindrical,
+    cylindrical_to_cartesian,
+    cartesian_to_spherical,
+    spherical_to_cartesian,
+    latitude_to_colatitude,
+    colatitude_to_latitude,
+)
 
 
 def test_cartensian_to_cylindrical():
     # a list of input argument (left) with the expected answer (right)
-    input_arguments_and_expected_result = np.array([
-        ((0, 0, 0), (0, 0, 0)),
-        ((1, 0, 0), (1, 0, 0)),
-        ((0, 1, 0), (1, pi / 2, 0)),
-        ((0, 0, 1), (0, 0, 1)),
-        ((-1, 0, 0), (1, pi, 0)),
-        ((0, -1, 0), (1, -pi / 2, 0)),
-        ((0, 0, -1), (0, 0, -1)),
-        ((-1, 1, 0), (sqrt(2), 3 * pi / 4, 0)),
-        ((1, -1, 0), (sqrt(2), -pi / 4, 0)),
-        ((0, 1, -1), (1, pi / 2, -1))
-    ])
+    input_arguments_and_expected_result = np.array(
+        [
+            ((0, 0, 0), (0, 0, 0)),
+            ((1, 0, 0), (1, 0, 0)),
+            ((0, 1, 0), (1, pi / 2, 0)),
+            ((0, 0, 1), (0, 0, 1)),
+            ((-1, 0, 0), (1, pi, 0)),
+            ((0, -1, 0), (1, -pi / 2, 0)),
+            ((0, 0, -1), (0, 0, -1)),
+            ((-1, 1, 0), (sqrt(2), 3 * pi / 4, 0)),
+            ((1, -1, 0), (sqrt(2), -pi / 4, 0)),
+            ((0, 1, -1), (1, pi / 2, -1)),
+        ]
+    )
 
     # apply all tests as float
     for argument, expected_result in input_arguments_and_expected_result:
@@ -31,14 +37,24 @@ def test_cartensian_to_cylindrical():
 
         # test for degrees as well
         result = cartesian_to_cylindrical(x, y, z, degrees=True)
-        expected_result_deg = [degrees(t) if i == 1 else t for i, t in enumerate(expected_result)]
+        expected_result_deg = [
+            degrees(t) if i == 1 else t for i, t in enumerate(expected_result)
+        ]
         assert_almost_equal([result], [expected_result_deg])
 
     # test array like result a well
-    input_arguments = input_arguments_and_expected_result[:, 0, :]  # select the input arguments
-    output_arguments = input_arguments_and_expected_result[:, 1, :]  # select the output arguments
+    input_arguments = input_arguments_and_expected_result[
+        :, 0, :
+    ]  # select the input arguments
+    output_arguments = input_arguments_and_expected_result[
+        :, 1, :
+    ]  # select the output arguments
     xx, yy, zz = input_arguments[:, 0], input_arguments[:, 1], input_arguments[:, 2]
-    rr_e, tt_e, zzc_e = output_arguments[:, 0], output_arguments[:, 1], output_arguments[:, 2]
+    rr_e, tt_e, zzc_e = (
+        output_arguments[:, 0],
+        output_arguments[:, 1],
+        output_arguments[:, 2],
+    )
     rr, tt, zzc = cartesian_to_cylindrical(xx, yy, zz)
     assert_almost_equal([rr], [rr_e])
     assert_almost_equal([tt], [tt_e])
@@ -52,18 +68,20 @@ def test_cartensian_to_cylindrical():
 
 def test_cylindrical_to_cartesian():
     # a list of input argument (left) with the expected answer (right)
-    input_arguments_and_expected_result = np.array([
-        ((0, 0, 0), (0, 0, 0)),
-        ((1, 0, 0), (1, 0, 0)),
-        ((1, pi / 2, 0), (0, 1, 0)),
-        ((0, 0, 1), (0, 0, 1)),
-        ((1, pi, 0), (-1, 0, 0)),
-        ((1, -pi / 2, 0), (0, -1, 0)),
-        ((0, 0, -1), (0, 0, -1)),
-        ((sqrt(2), 3 * pi / 4, 0), (-1, 1, 0)),
-        ((sqrt(2), -pi / 4, 0), (1, -1, 0)),
-        ((1, pi / 2, -1), (0, 1, -1))
-    ])
+    input_arguments_and_expected_result = np.array(
+        [
+            ((0, 0, 0), (0, 0, 0)),
+            ((1, 0, 0), (1, 0, 0)),
+            ((1, pi / 2, 0), (0, 1, 0)),
+            ((0, 0, 1), (0, 0, 1)),
+            ((1, pi, 0), (-1, 0, 0)),
+            ((1, -pi / 2, 0), (0, -1, 0)),
+            ((0, 0, -1), (0, 0, -1)),
+            ((sqrt(2), 3 * pi / 4, 0), (-1, 1, 0)),
+            ((sqrt(2), -pi / 4, 0), (1, -1, 0)),
+            ((1, pi / 2, -1), (0, 1, -1)),
+        ]
+    )
 
     # apply all tests
     for argument, expected_result in input_arguments_and_expected_result:
@@ -72,15 +90,24 @@ def test_cylindrical_to_cartesian():
 
         # test for degrees as well
         argument_deg = [degrees(t) if i == 1 else t for i, t in enumerate(argument)]
-        result = cylindrical_to_cartesian(argument_deg[0], argument_deg[1], argument_deg[2],
-                                          degrees=True)
+        result = cylindrical_to_cartesian(
+            argument_deg[0], argument_deg[1], argument_deg[2], degrees=True
+        )
         assert_almost_equal([result], [expected_result])
 
     # test array like result a well
-    input_arguments = input_arguments_and_expected_result[:, 0, :]  # select the input arguments
-    output_arguments = input_arguments_and_expected_result[:, 1, :]  # select the output arguments
+    input_arguments = input_arguments_and_expected_result[
+        :, 0, :
+    ]  # select the input arguments
+    output_arguments = input_arguments_and_expected_result[
+        :, 1, :
+    ]  # select the output arguments
     rr, tt, zzc = input_arguments[:, 0], input_arguments[:, 1], input_arguments[:, 2]
-    xx_e, yy_e, zz_e = output_arguments[:, 0], output_arguments[:, 1], output_arguments[:, 2]
+    xx_e, yy_e, zz_e = (
+        output_arguments[:, 0],
+        output_arguments[:, 1],
+        output_arguments[:, 2],
+    )
     xx, yy, zz = cylindrical_to_cartesian(rr, tt, zzc)
     assert_almost_equal([xx], [xx_e])
     assert_almost_equal([yy], [yy_e])
@@ -94,18 +121,20 @@ def test_cylindrical_to_cartesian():
 
 def test_cartesian_to_spherical():
     # a list of input argument (left) with the expected answer (right)
-    input_arguments_and_expected_result = np.array([
-        [[0, 0, 0], [0, 0, 0]],
-        [[1, 0, 0], [1, pi / 2, 0]],
-        [[0, 1, 0], [1, pi / 2, pi / 2]],
-        [[0, 0, 1], [1, 0, 0]],
-        [[-1, 0, 0], [1, pi / 2, pi]],
-        [[0, -1, 0], [1, pi / 2, -pi / 2]],
-        [[0, 0, -1], [1, pi, 0]],
-        [[-1, 1, 0], [sqrt(2), pi / 2, 3 * pi / 4]],
-        [[1, -1, 0], [sqrt(2), pi / 2, -pi / 4]],
-        [[0, 1, -1], [sqrt(2), 3 * pi / 4, pi / 2]]
-    ])
+    input_arguments_and_expected_result = np.array(
+        [
+            [[0, 0, 0], [0, 0, 0]],
+            [[1, 0, 0], [1, pi / 2, 0]],
+            [[0, 1, 0], [1, pi / 2, pi / 2]],
+            [[0, 0, 1], [1, 0, 0]],
+            [[-1, 0, 0], [1, pi / 2, pi]],
+            [[0, -1, 0], [1, pi / 2, -pi / 2]],
+            [[0, 0, -1], [1, pi, 0]],
+            [[-1, 1, 0], [sqrt(2), pi / 2, 3 * pi / 4]],
+            [[1, -1, 0], [sqrt(2), pi / 2, -pi / 4]],
+            [[0, 1, -1], [sqrt(2), 3 * pi / 4, pi / 2]],
+        ]
+    )
 
     # apply all tests
     for argument, expected_result in input_arguments_and_expected_result:
@@ -113,15 +142,27 @@ def test_cartesian_to_spherical():
         assert_almost_equal([result], [expected_result])
 
         # test for degrees as well
-        result = cartesian_to_spherical(argument[0], argument[1], argument[2], degrees=True)
-        expected_result_deg = [degrees(t) if i > 0 else t for i, t in enumerate(expected_result)]
+        result = cartesian_to_spherical(
+            argument[0], argument[1], argument[2], degrees=True
+        )
+        expected_result_deg = [
+            degrees(t) if i > 0 else t for i, t in enumerate(expected_result)
+        ]
         assert_almost_equal([result], [expected_result_deg])
 
     # test array like result a well
-    input_arguments = input_arguments_and_expected_result[:, 0, :]  # select the input arguments
-    output_arguments = input_arguments_and_expected_result[:, 1, :]  # select the output arguments
+    input_arguments = input_arguments_and_expected_result[
+        :, 0, :
+    ]  # select the input arguments
+    output_arguments = input_arguments_and_expected_result[
+        :, 1, :
+    ]  # select the output arguments
     xx, yy, zz = input_arguments[:, 0], input_arguments[:, 1], input_arguments[:, 2]
-    rr_e, tt_e, pp_e = output_arguments[:, 0], output_arguments[:, 1], output_arguments[:, 2]
+    rr_e, tt_e, pp_e = (
+        output_arguments[:, 0],
+        output_arguments[:, 1],
+        output_arguments[:, 2],
+    )
     rr, tt, pp = cartesian_to_spherical(xx, yy, zz)
     assert_almost_equal([rr], [rr_e])
     assert_almost_equal([tt], [tt_e])
@@ -135,18 +176,20 @@ def test_cartesian_to_spherical():
 
 def test_spherical_to_cartesian():
     # a list of input argument (left) with the expected answer (right)
-    input_arguments_and_expected_result = np.array([
-        ((0, 0, 0), (0, 0, 0)),
-        ((1, pi / 2, 0), (1, 0, 0)),
-        ((1, pi / 2, pi / 2), (0, 1, 0)),
-        ((1, 0, 0), (0, 0, 1)),
-        ((1, pi / 2, pi), (-1, 0, 0)),
-        ((1, pi / 2, -pi / 2), (0, -1, 0)),
-        ((1, pi, 0), (0, 0, -1)),
-        ((sqrt(2), pi / 2, 3 * pi / 4), (-1, 1, 0)),
-        ((sqrt(2), pi / 2, -pi / 4), (1, -1, 0)),
-        ((sqrt(2), 3 * pi / 4, pi / 2), (0, 1, -1))
-    ])
+    input_arguments_and_expected_result = np.array(
+        [
+            ((0, 0, 0), (0, 0, 0)),
+            ((1, pi / 2, 0), (1, 0, 0)),
+            ((1, pi / 2, pi / 2), (0, 1, 0)),
+            ((1, 0, 0), (0, 0, 1)),
+            ((1, pi / 2, pi), (-1, 0, 0)),
+            ((1, pi / 2, -pi / 2), (0, -1, 0)),
+            ((1, pi, 0), (0, 0, -1)),
+            ((sqrt(2), pi / 2, 3 * pi / 4), (-1, 1, 0)),
+            ((sqrt(2), pi / 2, -pi / 4), (1, -1, 0)),
+            ((sqrt(2), 3 * pi / 4, pi / 2), (0, 1, -1)),
+        ]
+    )
 
     # apply all tests
     for argument, expected_result in input_arguments_and_expected_result:
@@ -155,32 +198,45 @@ def test_spherical_to_cartesian():
 
         # test for degrees as well
         argument_deg = [degrees(t) if i > 0 else t for i, t in enumerate(argument)]
-        result = spherical_to_cartesian(argument_deg[0], argument_deg[1], argument_deg[2],
-                                        degrees=True)
+        result = spherical_to_cartesian(
+            argument_deg[0], argument_deg[1], argument_deg[2], degrees=True
+        )
         assert_almost_equal([result], [expected_result])
 
     # test array like result a well
-    input_arguments = input_arguments_and_expected_result[:, 0, :]  # select the input arguments
-    output_arguments = input_arguments_and_expected_result[:, 1, :]  # select the output arguments
+    input_arguments = input_arguments_and_expected_result[
+        :, 0, :
+    ]  # select the input arguments
+    output_arguments = input_arguments_and_expected_result[
+        :, 1, :
+    ]  # select the output arguments
     rr, tt, pp = input_arguments[:, 0], input_arguments[:, 1], input_arguments[:, 2]
-    xx_e, yy_e, zz_e = output_arguments[:, 0], output_arguments[:, 1], output_arguments[:, 2]
+    xx_e, yy_e, zz_e = (
+        output_arguments[:, 0],
+        output_arguments[:, 1],
+        output_arguments[:, 2],
+    )
     xx, yy, zz = spherical_to_cartesian(rr, tt, pp)
     assert_almost_equal([xx], [xx_e])
     assert_almost_equal([yy], [yy_e])
     assert_almost_equal([zz], [zz_e])
 
-    xx, yy, zz = spherical_to_cartesian(rr, np.rad2deg(tt), np.rad2deg(pp), degrees=True)
+    xx, yy, zz = spherical_to_cartesian(
+        rr, np.rad2deg(tt), np.rad2deg(pp), degrees=True
+    )
     assert_almost_equal([xx], [xx_e])
     assert_almost_equal([yy], [yy_e])
     assert_almost_equal([zz], [zz_e])
 
 
 def test_latitude_to_colatitude():
-    input_arguments_and_expected_result = np.array([
-        (0, pi / 2),
-        (pi / 4, pi / 4),
-        (pi / 2, 0),
-    ])
+    input_arguments_and_expected_result = np.array(
+        [
+            (0, pi / 2),
+            (pi / 4, pi / 4),
+            (pi / 2, 0),
+        ]
+    )
 
     for argument, expected_result in input_arguments_and_expected_result:
         result = latitude_to_colatitude(argument)
@@ -203,11 +259,13 @@ def test_latitude_to_colatitude():
 
 
 def test_colatitude_to_latitude():
-    input_arguments_and_expected_result = np.array([
-        (0, pi / 2),
-        (pi / 4, pi / 4),
-        (pi / 2, 0),
-    ])
+    input_arguments_and_expected_result = np.array(
+        [
+            (0, pi / 2),
+            (pi / 4, pi / 4),
+            (pi / 2, 0),
+        ]
+    )
 
     for argument, expected_result in input_arguments_and_expected_result:
         result = colatitude_to_latitude(argument)